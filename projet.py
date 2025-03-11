f28dc418db23cb60154f1472775678f23baa463e
rayen43500 <rayen.9b7@gmail.com>
2025-03-11 10:07:25 +0100

    last update

--- projet.py
+++ projet.py
@@ -1,15 +1,16 @@
 <<<<<<< HEAD
 def somme(T)
 S = 0
-
 for t in T:
 S +=t
 return S
 =======
+=======
+>>>>>>> branch1
 Data = [1,3,5]
 print("La somme est :", Som(Data))
 print("le min est:",min(Data))
 print("le max et :",max(Data))
 else:
 print("Liste vide")
->>>>>>> branch1
+
