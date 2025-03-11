3393601500769fd8173d363dfd29620b784d266a
rayen43500 <rayen.9b7@gmail.com>
2025-03-11 10:04:08 +0100

    last commit

--- projet.py
+++ projet.py
@@ -1,3 +1,4 @@
+
 <<<<<<< HEAD
 def somme(T)
 S = 0
@@ -5,13 +6,3 @@ S = 0
 for t in T:
 S +=t
 return S
-=======
-Data = [1,3,5]
-Som = sum(Data)
-print("La somme est :", Som)
->>>>>>> branch1
-=======
-Data = [1,3,5]
-Som = sum(Data)
-print("La somme est :", Som)
->>>>>>> branch1
