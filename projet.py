bf6956f3d0597d0d69284ac4e4f96373384338ea
rayen43500 <rayen.9b7@gmail.com>
2025-03-11 10:04:51 +0100

    last commit

--- projet.py
+++ projet.py
@@ -1,4 +1,3 @@
-
 <<<<<<< HEAD
 def somme(T)
 S = 0
@@ -6,3 +5,11 @@ S = 0
 for t in T:
 S +=t
 return S
+=======
+Data = [1,3,5]
+print("La somme est :", Som(Data))
+print("le min est:",min(Data))
+print("le max et :",max(Data))
+else:
+print("Liste vide")
+>>>>>>> branch1
